941bb1af68a31395a591c1f5659f6bc1cf87472f
finelee1119 <finelee1119@gmail.com>
2024-07-17 06:34:06 +0900

    #review 0716

---
+++ src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // HelloWorld 클래스의 범위 시작
+    public static void main(String[] args) { // main() 메서드의 범위 시작
+        System.out.println("Hello World");
+    } // main() 메서드의 범위 끝
+} // HelloWorld 클래스의 범위 끝
